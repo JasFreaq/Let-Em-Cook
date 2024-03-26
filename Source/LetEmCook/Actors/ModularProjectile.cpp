3f7ef198e740bc40c7ab8f8551e6b5a27cc6710e
Jasfiq Rahman <jasfiqrahman15@gmail.com>
2024-03-26 19:01:28 +0000

    Burger WIP

--- Source/LetEmCook/Actors/ModularProjectile.cpp
+++ Source/LetEmCook/Actors/ModularProjectile.cpp
@@ -8,53 +8,63 @@ void AModularProjectile::BeginPlay()
 {
 	Super::BeginPlay();
 
-	//TArray<USceneComponent*> MeshChildren;
-	//GetMesh()->GetChildrenComponents(true, MeshChildren);
+	TArray<USceneComponent*> MeshChildren;
+	GetMesh()->GetChildrenComponents(true, MeshChildren);
 
-	//Algo::Reverse(MeshChildren);
+	Algo::Reverse(MeshChildren);
 
-	//for (int i = 0; i < MeshChildren.Num(); i++)
-	//{
-	//	float ItemDistance = 0.0f;
+	for (int i = 0; i < MeshChildren.Num(); i++)
+	{
+		float ItemDistance = 0.0f;
+		
+		if (i > 0)
+		{
+			ItemDistance = MeshChildren[i]->GetRelativeLocation().Y - MeshChildren[i - 1]->GetRelativeLocation().Y;
+		}
+		else
+		{
+			ItemDistance = MeshChildren[i]->GetRelativeLocation().Y;
+		}
 
-	//	if (i > 0)
-	//	{
-	//		ItemDistance = MeshChildren[i]->GetRelativeLocation().Y - MeshChildren[i - 1]->GetRelativeLocation().Y;
-	//	}
-	//	else
-	//	{
-	//		ItemDistance = MeshChildren[i]->GetRelativeLocation().Y;
-	//	}
+		ItemDistances.Add(ItemDistance);
+	}
 
-	//	ItemDistances.Add(ItemDistance);
-	//}
+	// Setup
+	int ItemCounter = 0;
+	TArray<USceneComponent*> ChildrenStacked;
+	for (int i = 0; i < MeshChildren.Num(); i++)
+	{
+		USceneComponent* Child = MeshChildren[i];
 
-	//// Setup
-	//int ItemCounter = 0;
-	//for (int i = 0; i < MeshChildren.Num(); i++)
-	//{
-	//	USceneComponent* Child = MeshChildren[i];
+		if (i > 0)
+		{
+			if (Child->IsVisible())
+			{
+				FVector ChildRelativeLocation = Child->GetRelativeLocation();
+				FVector PrevChildLocation = ChildrenStacked.Num() > 0 ? ChildrenStacked[ItemCounter - 1]->GetRelativeLocation() : FVector::ZeroVector;
+				float NewYLocation = PrevChildLocation.Y + ItemDistances[i];
 
-	//	if (i > 0)
-	//	{
-	//		if (Child->IsVisible())
-	//		{
-	//			FVector ChildRelativeLocation = Child->GetRelativeLocation();
-	//			FVector PrevChildLocation = MeshChildren[ItemCounter]->GetRelativeLocation();
-	//			float NewYLocation = PrevChildLocation.Y + ItemDistances[ItemCounter];
+				FVector PrevChildBounds = MeshChildren[i - 1]->Bounds.BoxExtent;
+				FVector ChildBounds = ChildrenStacked.Num() > 0 ? ChildrenStacked[ItemCounter - 1]->Bounds.BoxExtent : FVector::ZeroVector;
 
-	//			ChildRelativeLocation.Y = NewYLocation;
+				NewYLocation += PrevChildBounds.Y - ChildBounds.Y;
 
-	//			Child->SetRelativeLocation(ChildRelativeLocation);
-	//			ItemCounter++;
-	//		}
-	//	}
-	//	else
-	//	{
-	//		if (Child->IsVisible())
-	//		{
-	//			ItemCounter++;
-	//		}
-	//	}
-	//}
+				UE_LOG(LogTemp, Warning, TEXT("Name: %s, Old: %f, New; %f, Dist: %f, Counter: %d"), *Child->GetName(), ChildRelativeLocation.Y, NewYLocation, ItemDistances[ItemCounter], ItemCounter);
+				ChildRelativeLocation.Y = NewYLocation;
+
+				Child->SetRelativeLocation(ChildRelativeLocation);
+
+				ChildrenStacked.Add(Child);
+				ItemCounter++;
+			}
+		}
+		else
+		{
+			if (Child->IsVisible())
+			{
+				ChildrenStacked.Add(Child);
+				ItemCounter++;
+			}
+		}
+	}
 }
